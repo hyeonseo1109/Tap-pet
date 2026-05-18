ff4e189f6a539c09ff8da93f59fe58de00eafa01
hyeonseo1109 <alsgustj2@naver.com>
2026-05-19 01:38:31 +0900

    feat: 프로젝트 시작

---
+++ src/pages/home/ui/index.tsx
@@ -0,0 +1,5 @@
+import * as styles from "./style.css";
+
+export const Home = () => {
+  return <div className={styles.homeContainer}>Home</div>;
+};
---
+++ src/pages/home/ui/style.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const homeContainer = style({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  color: "white",
+});
