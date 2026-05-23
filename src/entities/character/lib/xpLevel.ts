6472f53f83272c7b28a306c432967eb83965aab3
hyeonseo1109 <alsgustj2@naver.com>
2026-05-23 23:23:33 +0900

    design: 친구펫과 내 펫 위치 조절

--- src/entities/character/lib/xpLevel.ts
+++ src/entities/character/lib/xpLevel.ts
@@ -1,8 +1,8 @@
 import { CharacterStage } from "@entities/character/model";
 
 export const xpLevel = (xp: number): CharacterStage => {
-  if (xp < 10) return "egg";
-  if (xp < 50) return "baby";
-  if (xp < 100) return "child";
+  if (xp < 100) return "egg";
+  if (xp < 500) return "baby";
+  if (xp < 1000) return "child";
   return "adult";
 };
--- src/entities/character/ui/characterImage/style.css.ts
+++ src/entities/character/ui/characterImage/style.css.ts
@@ -2,11 +2,12 @@ import { style } from "@vanilla-extract/css";
 
 export const characterWidget = style({
   position: "fixed",
-  right: 20,
-  bottom: 30,
+  right: 10,
+  bottom: 15,
   width: 52,
   height: 64,
   backgroundRepeat: "no-repeat",
   imageRendering: "pixelated",
   transform: "scale(1.5)",
+  transformOrigin: "bottom right",
 });
